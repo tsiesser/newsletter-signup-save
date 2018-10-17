63b140ca42d1c18a3ed42914439deb5c2659ce49
tracey <tsiesser@yahoo.com>
2018-10-17 17:09:48 -0400

    Updates

---
+++ precache-manifest.eac8eaaeb018bf1451bd83ac5e0f3e72.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "c202697535db828b33bfedab17d6b26a",
+    "url": "/newsletter-signup/static/media/Azo-Sans-Bold.c2026975.woff"
+  },
+  {
+    "revision": "a4095ac6766a8e3f5f3c",
+    "url": "/newsletter-signup/static/css/main.fc8477e1.chunk.css"
+  },
+  {
+    "revision": "c22bf75bee9499c523fb",
+    "url": "/newsletter-signup/static/js/1.c22bf75b.chunk.js"
+  },
+  {
+    "revision": "703edb52e9b1bfbf9128",
+    "url": "/newsletter-signup/static/js/runtime~main.703edb52.js"
+  },
+  {
+    "revision": "f55d13a16f31a83a508e557dc19c82df",
+    "url": "/newsletter-signup/static/media/Elaine_D02.f55d13a1.woff"
+  },
+  {
+    "revision": "3aadfe76191eb3880272658959119c66",
+    "url": "/newsletter-signup/static/media/Elaine_D02.3aadfe76.svg"
+  },
+  {
+    "revision": "0e7a9d1bb0b51587bc868e9549d95195",
+    "url": "/newsletter-signup/static/media/Azo-Sans.0e7a9d1b.eot"
+  },
+  {
+    "revision": "88ed9b8a1c42e366d81ed994320ac23b",
+    "url": "/newsletter-signup/static/media/Azo-Sans.88ed9b8a.woff"
+  },
+  {
+    "revision": "b8344010b32da9f6eff4c22cf377fb03",
+    "url": "/newsletter-signup/static/media/Azo-Sans.b8344010.ttf"
+  },
+  {
+    "revision": "07c852863cd251e2bd27ad8fd6bbae9f",
+    "url": "/newsletter-signup/static/media/Azo-Sans-Bold.07c85286.eot"
+  },
+  {
+    "revision": "a4095ac6766a8e3f5f3c",
+    "url": "/newsletter-signup/static/js/main.a4095ac6.chunk.js"
+  },
+  {
+    "revision": "5cd5dd7d40c2c83c1e87e3d95815b844",
+    "url": "/newsletter-signup/static/media/Azo-Sans.5cd5dd7d.svg"
+  },
+  {
+    "revision": "ca2d2cc1b9f9e25dbd5e0ffa9d6365ea",
+    "url": "/newsletter-signup/static/media/Azo-Sans-Bold.ca2d2cc1.svg"
+  },
+  {
+    "revision": "53962c38395b376ec3e211d9291dc150",
+    "url": "/newsletter-signup/static/media/Azo-Sans-Bold.53962c38.ttf"
+  },
+  {
+    "revision": "fce716b4433e24785ba6ee22fe70c6c3",
+    "url": "/newsletter-signup/static/media/Azo-Sans-Light.fce716b4.woff"
+  },
+  {
+    "revision": "d670362ae4281d629d46cd73cea80447",
+    "url": "/newsletter-signup/static/media/Azo-Sans-Light.d670362a.eot"
+  },
+  {
+    "revision": "d817e82f00ca0e949be73908057c9c7e",
+    "url": "/newsletter-signup/static/media/Elaine_D02.d817e82f.eot"
+  },
+  {
+    "revision": "7b7d4e516c3664470125ccec36a895db",
+    "url": "/newsletter-signup/static/media/Elaine_D02.7b7d4e51.ttf"
+  },
+  {
+    "revision": "995dc41b87b6477083859716a94105f0",
+    "url": "/newsletter-signup/static/media/Azo-Sans-Light.995dc41b.ttf"
+  },
+  {
+    "revision": "da4cc610538ad56eede4e66f81940520",
+    "url": "/newsletter-signup/static/media/Azo-Sans-Light.da4cc610.svg"
+  },
+  {
+    "revision": "b28804c2d0ce563236a59af96eaaa6a0",
+    "url": "/newsletter-signup/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/newsletter-signup/precache-manifest.eac8eaaeb018bf1451bd83ac5e0f3e72.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/newsletter-signup/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
